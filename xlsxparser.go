97657026e3c3fa45b8cd979e5f48b141c14c1a52
Case Talbot <casetalbot@gmail.com>
2020-03-22 23:03:17 -0700

    Actually Title Case

--- xlsxparser.go
+++ xlsxparser.go
@@ -62,7 +62,7 @@ func (p *Parser) parseSheet(sheetKey string) ([]ProductRow, []error) {
 		if row[0] == "Product Name" {
 			continue
 		}
-		product.ProductName = strings.Title(row[0])
+		product.ProductName = strings.Title(strings.ToLower(row[0]))
 		product.Category = row[1]
 		price, err := strconv.ParseFloat(row[2], 64)
 		if err != nil {
